a8230425e6849cccbb1f5d37f12d7aa7a02c6d91
Pau1824 <chaviraespinozapaulina@gmail.com>
2025-03-12 23:58:27 -0700

    ups

---
+++ practicas/validaciones-sh/obtener_version.sh
@@ -0,0 +1,22 @@
+source "./variables/variables_http.sh"
+
+obtener_version(){
+    local service="$1"
+    case "$service" in
+        Apache)
+            versions=$(curl -s "$url_apache" |  grep -oP '(?<=Apache HTTP Server )\d+\.\d+\.\d+' | sort -V | uniq)
+            ;;
+        Nginx)
+            versions=$(curl -s "$url_nginx" |  grep -oP '(?<=nginx-)\d+\.\d+\.\d+' | sort -V | uniq)
+            ;;
+        OpenLiteSpeed)
+            versions=$(curl -s "$url_litespeed" | grep -oP 'openlitespeed-\d+\.\d+\.\d+' | sort -V | uniq)
+            ;;
+        *)
+            echo "Servicio no soportado"
+            exit 1
+            ;;
+    esac
+
+    echo "$versions"
+}
---
+++ practicas/validaciones-sh/variables_http.sh
@@ -0,0 +1,6 @@
+VARIABLES UTILIZADAS: url_apache="https://httpd.apache.org/download.cgi"
+url_nginx="https://nginx.org/en/download.html"
+url_litespeed="https://openlitespeed.org/downloads/"
+url_apache_descargas="https://downloads.apache.org/httpd/"
+url_litespeed_descargas="https://openlitespeed.org/packages/"
+url_nginx_descargas="https://nginx.org/download/"
